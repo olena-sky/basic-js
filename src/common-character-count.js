a70dd032da45a5ef9f1f16f1c538c377cbb28055
Olena Stadnyk <96227346+olenaxx@users.noreply.github.com>
2023-12-19 09:35:38 +0200

    add implemented task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const charCount1 = countCharacters(s1);
+  const charCount2 = countCharacters(s2);
+
+  let count = 0;
+
+  for (const char in charCount1) {
+    if (charCount2.hasOwnProperty(char)) {
+      count += Math.min(charCount1[char], charCount2[char]);
+    }
+  }
+
+  return count;
+}
+
+function countCharacters(str) {
+  const charCount = {};
+
+  for (const char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  return charCount;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,10 +11,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  return n
 }
+// function deleteDigit(/* n */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
 
 module.exports = {
   deleteDigit
