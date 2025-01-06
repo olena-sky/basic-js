caeb6ff0d4b6966384f2073fef61b6977ed9fa99
Olena <olena.lightness@gmail.com>
2025-01-07 00:49:41 +0200

    implement sum-digits

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,6 +16,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
+// function getMatrixElementsSum(/* matrix */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
 function getMatrixElementsSum(/* matrix */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
